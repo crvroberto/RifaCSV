5dcd40118f1691424b267493a955e491abe44085
Carlos Vieira <carlos010442@hotmail.com>
2021-05-23 01:03:57 -0300

    firt commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
 import './App.css';
+import { CSVLink, CSVDownload } from "react-csv";
 
 function App() {
+
+  
+  function returnNumber(init, end) {
+
+    let array = []
+    for (let i = init; i <= end; i++){ 
+      let str = "" + i
+      let pad = "0000"
+     str = pad.substring(0, pad.length - str.length) + str
+      array.push([str])
+    }
+    
+  return array.sort(()=> 0.5 - Math.random())
+   
+  }
+  
+  const a = returnNumber(0,1999)
+  const b = returnNumber(2000,3999)
+  const c = returnNumber(4000,5999)
+  const d = returnNumber(6000,7999)
+  const e = returnNumber(8000,9999)
+  a.unshift(["a"])
+  b.unshift(["b"])
+  c.unshift(["c"])
+  d.unshift(["d"])
+  e.unshift(["e"])
+console.log(a)
+  const numbers = []
+
+  for (let i = 0; i < 2001; i++){
+
+   numbers.push(a[i].concat(b[i],c[i],d[i],e[i]))
+  }
+
+
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <CSVLink
+  data={numbers}
+  filename={"my-file.csv"}
+  className="btn btn-primary"
+  target="_blank"
+    separator={';'}
+>
+  Download me
+</CSVLink>
+   
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
